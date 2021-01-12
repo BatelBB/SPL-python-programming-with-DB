5ad4f207de2aecf641047f230fcc1d6f6481a5a2
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-01-12 11:29:14 +0200

    Added output and order args and changed the reading of the first line

--- main.py
+++ main.py
@@ -1,59 +1,68 @@
-import _sqlite3
-import os
-import atexit
 import sys
 import Repository
+from Orders import executeOrders
 
 from pluralObjects.Summaries import Summaries
+from pluralObjects.Suppliers import Suppliers
 from singleObjects.Clinic import Clinic
 from singleObjects.Vaccine import Vaccine
 from singleObjects.Supplier import Supplier
 from singleObjects.Logistic import Logistic
 
 
-def add_information_to_tables(filePath, repo):
-    with open(filePath) as file:
+def add_information_to_tables(filePath, repo, sumVaccineAmount, sumClinicsDemand):
+    with open(filePath, 'r', encoding='utf-8') as file:
+        #splitFirstLine = file.readline(0).replace(' ', '')
+        #splitFirstLine = file.readline(0).split(',')
+        #test = list(map(int, splitFirstLine))
 
-        splitFirstLine = file.readline(0).split(',')
-        amountVaccines = int(splitFirstLine[0])
-        amountSuppliers = int(splitFirstLine[1])
-        amountClinics = int(splitFirstLine[2])
-        amountLogistics = int(splitFirstLine[3])
+        for index in range(0, 1):
+            line = next(file)
+            splitFirstLine = line.split(',')
+            amountVaccines = int(splitFirstLine[0])
+            amountSuppliers = int(splitFirstLine[1])
+            amountClinics = int(splitFirstLine[2])
+            amountLogistics = int(splitFirstLine[3])
 
         for index in range(1, amountVaccines + 1):
             line = next(file)
             splitVaccines = line.split(',')
-            vaccinesFromFile = Vaccine(splitVaccines[1], splitVaccines[2], splitVaccines[3])
-            sumVaccineAmount = sumVaccineAmount + splitVaccines[2]
-            repo.vaccines.insert(vaccinesFromFile)
+            vaccinesFromFile = Vaccine(splitVaccines[1], int(splitVaccines[2]), splitVaccines[3])
+            sumVaccineAmount = sumVaccineAmount + int(splitVaccines[2])
+            repo.Vaccines.insert(vaccinesFromFile)
 
         for index in range(amountVaccines + 1, amountSuppliers + 1):
             line = next(file)
             splitSuppliers = line.split(',')
             supplierFromFile = Supplier(splitSuppliers[0], splitSuppliers[1], splitSuppliers[2])
-            repo.supplier.insert(supplierFromFile)
+            repo.Suppliers.insert(supplierFromFile)
 
         for index in range(amountSuppliers + 1, amountClinics + 1):
             line = next(file)
             splitClinic = line.split(',')
-            clinicsFromFile = Clinic(splitClinic[0], splitClinic[1], splitClinic[2], splitClinic[3])
-            sumClinicsDemand = sumClinicsDemand + splitClinic[2]
-            repo.clinic.insert(clinicsFromFile)
+            clinicsFromFile = Clinic(splitClinic[0], splitClinic[1], int(splitClinic[2]), splitClinic[3])
+            sumClinicsDemand = sumClinicsDemand + int(splitClinic[2])
+            repo.Clinics.insert(clinicsFromFile)
 
         for index in range(amountClinics + 1, amountLogistics + 1):
             line = next(file)
             splitLogistics = line.split(',')
             logisticFromFile = Logistic(splitLogistics[0], splitLogistics[1], splitLogistics[2], splitLogistics[3])
-            repo.logistic.insert(logisticFromFile)
+            repo.Logistics.insert(logisticFromFile)
         Summaries.__init__(sumVaccineAmount, sumClinicsDemand, 0, 0)
 
 
 def main(args):
-    inputfilename = args[1]
+    configFile = args[1]
+    orderFile = args[2]
+    outputFile = args[3]
     repo = Repository.repo
     repo.__init__()
     repo.create_tables()
-    add_information_to_tables(inputfilename, repo)
+    add_information_to_tables(configFile, repo, 0, 0)
+    executeOrders(orderFile, repo)
+    Summaries.createOutput(outputFile)
+
 
 if __name__ == '__main__':
     main(sys.argv)
