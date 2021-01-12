e778961885fffdfe354244e26115d282b82d7d8a
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-01-12 11:30:39 +0200

    Deleted id

--- singleObjects/Vaccine.py
+++ singleObjects/Vaccine.py
@@ -1,7 +1,6 @@
 # Data Transfer Objects:
 class Vaccine:
-    def __init__(self, id, date, supplier, quantity):
-        self.id = id
+    def __init__(self, date, supplier, quantity):
         self.date = date
         self.supplier = supplier
         self.quantity = quantity
